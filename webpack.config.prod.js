1a527d4f506580d798eb31cabf9d36e0b58dd66f
Olu Ayandosu <oayandosu@gmail.com>
2016-03-02 11:32:14 -0700

    Updated webpack config for builds

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,9 +1,10 @@
-var path          = require('path')
-var webpack       = require('webpack')
-var autoprefixer  = require('autoprefixer')
+var path              = require('path')
+var webpack           = require('webpack')
+var autoprefixer      = require('autoprefixer')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
-  devtool: 'source-map',
+  // devtool: 'cheap-module-source-map',
   entry: [
     './src/index'
   ],
@@ -23,7 +24,26 @@ module.exports = {
   },
   plugins: [
     new webpack.NoErrorsPlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
+
+    new ExtractTextPlugin("[name].css"),
+
+    // This plugin looks for similar chunks and files
+    // and merges them for better caching by the user
+    new webpack.optimize.DedupePlugin(),
+
+    // This plugins optimizes chunks and modules by
+    // how much they are used in your app
+    // new webpack.optimize.OccurenceOrderPlugin(),
+
+    // This plugin minifies all the Javascript code of the final bundle
+    // new webpack.optimize.UglifyJsPlugin({
+    //   mangle: true,
+    //   sourceMap: false,
+    //   compress: {
+    //     warnings: false, // Suppress uglification warnings
+    //   },
+    // }),
+
     new webpack.DefinePlugin({
       'process.env': {
         'NODE_ENV': JSON.stringify('production')
@@ -33,19 +53,17 @@ module.exports = {
   module: {
     loaders: [
       {
+        test:   /\.css$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader', 'postcss-loader')
+      },
+      {
         test: /\.js$/,
         loader: 'babel',
         exclude: /(node_modules|example)/
       },
-      {
-        test:   /\.css$/,
-        loader: "style-loader!css-loader!postcss-loader"
-      }
     ]
   },
-  postcss: function (webpack) {
-    return [
-      autoprefixer
-    ]
+  postcss: function () {
+    return [autoprefixer]
   }
 }
